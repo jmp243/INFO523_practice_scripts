e9c0441ecafafdc06ca9acc8fc6e7e726e5e930d
Jung Mee Park <jpark1917@gmail.comecho>
2021-11-06 09:18:34 -0700

    new ones

--- HW_4_Park_JM.R
+++ HW_4_Park_JM.R
@@ -31,7 +31,8 @@ ggplot(titanic, aes(Class, Freq)) +
 
 # 6. Use the geom_jitter
 ggplot(titanic, aes(Class, Freq)) +
-  geom_boxplot() 
+  geom_boxplot() +
+  geom_jitter()
 
 # 7. set width and alpha set to width = 0.25, alpha=0.5
 ggplot(titanic, aes(Class, Freq)) +
@@ -66,14 +67,15 @@ train_titanic <- read.csv("train.csv")
 
 # 1.	What was the survival frequency on Titanic? 
 # Examine this question from the following variables: gender, class, age, and ticket fare. 
-violin_first <- ggplot(train_titanic, aes(x = Survived, y = Age, fill = Sex)) +
-  geom_violin() +
-  ylab("") +
-  xlab("")  
-  # stat_summary(fun = "mean", geom = "point", shape = 8, size = 3, color = "midnightblue") 
 
-violin_first <- violin_first +  labs(title = "", 
-                                     subtitle = "") 
+violin_first <- ggplot(train_titanic, aes(x = Survived, y = Fare, fill=Sex)) +
+  geom_violin() +
+  ylab("cost of fare") +
+  xlab("") +
+  stat_summary(fun = "mean", geom = "point", shape = 8, size = 3, color = "midnightblue") 
+
+violin_first <- violin_first +  labs(title = "Violin plot of Survival by fare and gender", 
+                                     subtitle = "training dataset") 
 print(violin_first)
 
 ####
@@ -84,10 +86,21 @@ library(psych)
 library(Amelia)
 
 # assign as factor variables
-train_titanic$Survived <- as.factor(train_titanic$Survived)
-train_titanic$Pclass <- as.factor(train_titanic$Pclass, order=TRUE, levels=c(3,2,1))
+# train_titanic$Survived <- as.factor(train_titanic$Survived)
+train_titanic$Survived  <- recode_factor(train_titanic$Survived , "1" = "Survived", "0" = "Died")
+
+train_titanic$Pclass <- factor(train_titanic$Pclass, levels = c("3","2","1"), 
+                                  ordered = TRUE)
 train_titanic$PassengerId <- as.factor(train_titanic$PassengerId)
 train_titanic$Cabin <- as.factor(train_titanic$Cabin)
+train_titanic$char_cabin <- as.character(train_titanic$Cabin)     
+
+new_Cabin <- ifelse(train_titanic$char_cabin == "",          
+                    "",                        
+                    substr(train_titanic$char_cabin,1,1))    
+
+new_Cabin <- factor(new_Cabin )                
+train_titanic$Cabin <- new_Cabin
 # train_titanic$Age <- as.numeric(train_titanic$Age)
 
 # prepare missing variables 
@@ -123,7 +136,7 @@ gender_stack <- train_titanic %>%
   # geom_text(stat = 'count',
   #           aes(label=stat(count)),
   #           position = position_dodge(width = 1), vjust=-0.5) + 
-  xlab("0 = Dead, 1 = Survived") 
+  xlab("outcome") 
 
 gender_stack <- gender_stack + labs(title = "Survival count by sex",
                                 # subtitle = "training dataset", 
@@ -139,7 +152,7 @@ class_graph <- ggplot(train_titanic, aes(x=Survived, fill=Pclass)) +
   geom_text(stat = 'count',
             aes(label=stat(count)),
             position = position_dodge(width = 1), vjust=-0.5) + 
-  xlab("0 = Dead, 1 = Survived") 
+  xlab("outcome") 
 
 
 class_graph <- class_graph + labs(title = "Survival count by class",
@@ -158,7 +171,7 @@ print(age_density)
 
 # survival by age
 train_titanic$Discretized.age <- cut(train_titanic$Age, c(0,10,20,30,40,50,60,70,80,100))
-train_titanic$Discretized.age = NULL
+# train_titanic$Discretized.age = NULL
 # Plot discretized age
 age_bar <- ggplot(train_titanic, aes(x = Discretized.age, fill=Survived)) +
   geom_bar(position = position_dodge()) +
@@ -195,16 +208,33 @@ t.test(, var.equal = TRUE) # this did not work.
 # go over the figures first 
 
 # aggregate for age
-tabl <- aggregate(Age~Survived,data=titanic,FUN = mean)
+tabl <- aggregate(Age~Survived,data=train_titanic,FUN = mean)
 tabl
 
 # 2.	What was the order of priority (i.e. gender and age) to rescue passengers of Titanic?
 # women and children and then 1st, 2nd, 3rd class individuals
 # train_titanic$Survived ~ train_titanic$Sex
+violin_two <- ggplot(train_titanic, aes(x = Survived, y = Fare)) +
+  geom_violin() +
+  ylab("cost of fare") +
+  stat_summary(fun = "mean", geom = "point", shape = 8, size = 3, color = "midnightblue") 
+
+violin_two <- violin_two +  labs(title = "Violin plot of Survival by fare", 
+                                 subtitle = "training dataset") 
+print(violin_two)
+
+# looking at age
+violin_age <- ggplot(train_titanic, aes(x = Survived, y = Age, fill = Sex)) +
+  geom_violin() +
+  # stat_summary(fun = "mean", geom = "point", shape = 8, size = 3, color = "midnightblue") +
+  ylab("Age") 
+violin_age <- violin_age +  labs(title = "Violin plot of Survival by age and sex", 
+                                 subtitle = "training dataset") 
+print(violin_age)
 
 # subset data into survivor and nonsurvivor
-titanic_survive <- train_titanic[train_titanic$Survived == 1, ]
-titanic_die <- train_titanic[train_titanic$Survived == 0, ]
+# titanic_survive <- train_titanic[train_titanic$Survived == 1, ]
+# titanic_die <- train_titanic[train_titanic$Survived == 0, ]
 
 
 # library(rpart)
@@ -212,6 +242,32 @@ titanic_die <- train_titanic[train_titanic$Survived == 0, ]
 # 
 # fit <- rpart(Survived ~ ., data = train_titanic, method = 'class')
 # rpart.plot(fit) # this does not look good. 
+
+titanic_model <- glm(Survived ~ Sex,        # Formula
+                     data= train_titanic,    # Data set
+                     family="binomial")      # family="binomial" for binary logistic
+
+summary(titanic_model)
+
+# full model
+full_titantic_model <- glm(Survived ~ Sex+Pclass+Age+SibSp+Parch+Fare,
+                     data= train_titanic,       
+                     family="binomial")         
+
+summary(full_titantic_model)
+
+# calculate predicted probabilities
+predicted_probabilities <- predict(full_titantic_model,              
+                                   newdata=train_titanic,      
+                                   type="response")   
+### Convert to 0, 1 preds
+class_preds <- ifelse(predicted_probabilities >= 0.5, 1, 0)  
+
+# Make a table of predictions vs. actual
+result_table <- table(class_preds,             
+                      train_titanic$Survived)  
+
+result_table
 
 # 3.	What were the characteristics of the passengers that did not survive? 
 # younger men who paid lower fares did not survive
@@ -225,12 +281,12 @@ pct_graph1 <-
   ggplot(overview_pct1, aes(Survived, 
                             count, fill = Survived)) +
   geom_bar(stat='identity') +
-  labs(x = "", y = "") +
+  labs(x = "0 = Dead, 1 = Survived", y = "") +
   coord_flip() +
   # theme(legend.position="none") +
   geom_text(aes(label = scales::percent(pct), y = if_else(count > 0.1*max(count), count/2, count+ 0.05*max(count))))
 
-pct_graph1 <- pct_graph1 + labs(title = "", 
+pct_graph1 <- pct_graph1 + labs(title = "Overall Survival Percentage", 
                                 subtitle = "training dataset",  fill = "Class")
 
 print(pct_graph1)
@@ -238,11 +294,7 @@ print(pct_graph1)
 # survived, class, 
 # https://www.kaggle.com/hamelg/intro-to-r-part-28-logistic-regression
 
-titanic_model <- glm(Survived ~ Sex,        # Formula
-                     data= train_titanic,    # Data set
-                     family="binomial")      # family="binomial" for binary logistic
 
-summary(titanic_model)
 # 4.	Do you notice any interesting patterns in survival frequencies 
 # based on the place where passengers boarded? (e.g. where did most 
 # passengers board? Is there any difference in survival for passengers 
@@ -254,11 +306,32 @@ embark_graph <- ggplot(train_titanic, aes(x=Survived, fill=Embarked)) +
   geom_text(stat = 'count',
             aes(label=stat(count)),
             position = position_dodge(width = 1), vjust=-0.5) + 
-  xlab("0 = Dead, 1 = Survived") 
+  xlab("outcome") 
 
 
 embark_graph <- embark_graph + labs(title = "Survival count by port of embarkment",
                                   subtitle = "training dataset", 
                                   fill = "Port")
 print(embark_graph)
+
+# 5.	Was it a good idea to travel in big groups with parents/children or 
+# was it better to travel in small groups with siblings/spouses?
+
+violin_sib <- ggplot(train_titanic, aes(x = Survived, y = SibSp, fill = Sex)) +
+  geom_violin() +
+  # stat_summary(fun = "mean", geom = "point", shape = 8, size = 3, color = "midnightblue") +
+  ylab("Sibling and Spouse") 
+violin_sib <- violin_sib +  labs(title = "Violin plot of Survival", 
+                                 subtitle = "training dataset") 
+print(violin_sib)
+
+# violin for 
+
+violin_parch <- ggplot(train_titanic, aes(x = Survived, y = Parch, fill = Sex)) +
+  geom_violin() +
+  # stat_summary(fun = "mean", geom = "point", shape = 8, size = 3, color = "midnightblue") +
+  ylab("Parent and Child") 
+violin_parch <- violin_parch +  labs(title = "Violin plot of Survival", 
+                                 subtitle = "training dataset") 
+print(violin_parch)
 
