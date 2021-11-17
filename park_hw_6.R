aa4609384c021a62dcd9861401bbf7a2a31f2d37
Jung Mee Park <jpark1917@gmail.comecho>
2021-11-16 20:20:37 -0700

    hw6

---
+++ park_hw_6.R
@@ -0,0 +1,15 @@
+# Jung Mee Park
+# jmpark@arizona.edu
+# INFO 523 
+# Homework 6
+# 2021-15-11
+
+# load packages
+library(RMySQL)
+library(DBI)
+
+# chapter on Detecting Fraudulent Transactions
+library(dplyr)
+library(DMwR2)
+library(ggplot2)
+library(forcats)
